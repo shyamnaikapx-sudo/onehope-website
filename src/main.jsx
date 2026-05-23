9ea9253490986a463019c7b6051baf1643341acf
shyamnaikapx-sudo <shyamnaikapx@gmail.com>
2026-05-23 09:43:16 +0530

    Fixed routing and homepage

--- src/main.jsx
+++ src/main.jsx
@@ -1,31 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
 import {
   BrowserRouter,
-  Routes,
-  Route,
 } from "react-router-dom";
 
 import App from "./App";
-import AuditSimulator from "./pages/AuditSimulator";
-
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 
   <BrowserRouter>
 
-    <Routes>
-
-      <Route path="/" element={<App />} />
-
-      <Route
-        path="/ai-audit-simulator"
-        element={<AuditSimulator />}
-      />
-
-    </Routes>
+    <App />
 
   </BrowserRouter>
 
